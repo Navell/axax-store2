55100063d9c2dc663b4c1017ea5c7a8cb62c607b
Navel <you@example.com>
2024-08-13 16:57:52 +0300

    delete gitignore

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-  import HeaderItem from './components/HeaderItem/HeaderItem.vue'
-  import MainPage from './MainPage/MainPage.vue'
+  import  HeaderItem from './components/HeaderItem/HeaderItem.vue'
+  import *as MainPage from './MainPage/MainPage.vue'
+  
 
 </script>
 
--- src/MainPage/MainPage.vue
+++ src/MainPage/MainPage.vue
@@ -1,7 +1,7 @@
 <script lang='ts'>
     // import "./main-page"
-    import ButtonControl from '../components/controls/ButtonControl.vue'
-    import HeroPage from './Pages/HeroPage/HeroPage.vue'
+    // import *as ButtonControl from '../components/controls/ButtonControl.vue'
+    import HeroPage from './Pages/HeroPage/HeroPage'
 </script>
 
 <template>
